40d339cf25a68b4a870b4601103ff04fde46fd43
alexnderKirov43 <region-manager43@yandex.ru>
2024-03-04 01:04:42 +0300

    first commit

---
+++ JS/sideBar.js
@@ -0,0 +1,7 @@
+function openSideBar(){
+    document.getElementById("sideBar").classList.add('sideBar__show')
+};
+
+function sideBarCloce(){
+    document.getElementById("sideBar").classList.remove('sideBar__show')
+};
