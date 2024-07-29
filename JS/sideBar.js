5029b91f52f2b75ffb4e4ba8ec4ed84066e23fbc
aleksanderKirov43 <region-manager43@yandex.ru>
2024-07-29 13:01:29 +0300

    create form

--- JS/sideBar.js
+++
@@ -1,7 +0,0 @@
-function openSideBar(){
-    document.getElementById("sideBar").classList.add('sideBar__show')
-};
-
-function sideBarCloce(){
-    document.getElementById("sideBar").classList.remove('sideBar__show')
-};
